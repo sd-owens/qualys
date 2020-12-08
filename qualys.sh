4db4483ee9f9485ddef9e7307dc53968c1d30095
Steve Owens <sd.owens@outlook.com>
2020-12-07 20:51:53 -0800

    working GNU date variables NOW and -90 days

--- qualys.sh
+++ qualys.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
 
-# isCompliant(){
+isCompliant(){
 
-#     # NOW=$(date + %m-%d-%Y) # GNU
-#     NOW=$(date +"%m-%d-%Y") # BSD
+    # GNU format
+    NOW=$(date +%m-%d-%Y)
+    # BSD format
+    # NOW=$(date +"%m-%d-%Y")
 
-#     # GNU data format
-#     # THEN=$(date --date="$date -90 days" +"%m-%d-%Y") 
-
-#     # BSD data format
-#     _90DAYS=$(date -j -v -90d +"%m-%d-%Y")
-
-
-#     echo $_NOW
-#     echo $_90DAYS
-
-#     if NOW
-#     then
-#         # True = 1
-#         return 1
-
-#     else
-#         # False = 0
-#         return 0
-#     fi
-
-# }
+    # GNU format
+    THEN=$(date --date="$date -90 day" +%m-%d-%Y) 
+    # BSD data format
+    # THEN=$(date -j -v -90d +"%m-%d-%Y")
 
 
-depth=0
+    echo $NOW
+    echo $THEN
 
-while find -mindepth $depth -maxdepth $depth | grep '.'
-do
-    depth=$((depth + 1))
-done
+    if NOW
+    then
+        # True = 1
+        return 1
+
+    else
+        # False = 0
+        return 0
+    fi
+
+}
+
+
+# depth=0
+
+# while find -mindepth $depth -maxdepth $depth | grep '.'
+# do
+#     depth=$((depth + 1))
+# done
 
 
 
 
-# if isCompliant;
-# then echo "isCompliant";
-# else echo "notCompliant";
-# fi
+if isCompliant;
+then echo "isCompliant";
+else echo "notCompliant";
+fi
